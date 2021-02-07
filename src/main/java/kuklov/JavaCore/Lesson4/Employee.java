d9cb00ec6ab6f2780b9037aa07784e32ebdfd26c
Garry Deepcool <garrydeepcool@outlook.com>
2021-02-08 01:42:06 +0500

    Class 'Employee' added.

--- src/main/java/kuklov/JavaCore/Lesson4/Employee.java
+++ src/main/java/kuklov/JavaCore/Lesson4/Employee.java
@@ -6,4 +6,25 @@ public class Employee {
     public String surname;
     public int age;
     public String title;
+    public int phoneNumber;
+    public String email;
+    public double salary;
+
+    Employee(String name, String name2, String surname, int age, String title, int phoneNumber, String email, double salary )  {
+        this.name = name;
+        this.name2 = name2;
+        this.surname = surname;
+        this.age = age;
+        this.title = title;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.salary = salary;
+    }
+
+    public void showInfo()  {
+        System.out.printf("Name, age: \s \s \s \f \n", name, name2, surname, age);
+        System.out.printf("Title: \s\n", title);
+        System.out.printf("Email, phone: \s\t \f \n", email, phoneNumber);
+        System.out.printf("Salary: \f\n", salary);
+    }
 }
