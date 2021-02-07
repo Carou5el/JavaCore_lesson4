2e33eb7d5d4f2eb4eaced2ce2a2868b63211618f
Garry Deepcool <garrydeepcool@outlook.com>
2021-02-08 02:14:20 +0500

    Array 'emps' added.

--- src/main/java/kuklov/JavaCore/Lesson4/Employee.java
+++ src/main/java/kuklov/JavaCore/Lesson4/Employee.java
@@ -8,9 +8,9 @@ public class Employee {
     public String title;
     public int phoneNumber;
     public String email;
-    public double salary;
+    public int salary;
 
-    Employee(String name, String name2, String surname, int age, String title, int phoneNumber, String email, double salary )  {
+    Employee(String name, String name2, String surname, int age, String title, int phoneNumber, String email, int salary )  {
         this.name = name;
         this.name2 = name2;
         this.surname = surname;
@@ -22,9 +22,10 @@ public class Employee {
     }
 
     public void showInfo()  {
-        System.out.printf("Name, age: \s \s \s \f \n", name, name2, surname, age);
-        System.out.printf("Title: \s\n", title);
-        System.out.printf("Email, phone: \s\t \f \n", email, phoneNumber);
-        System.out.printf("Salary: \f\n", salary);
+        System.out.printf("==========================================================\n");
+        System.out.printf("Name, age: %s %s %s %d\n", name, name2, surname, age);
+        System.out.printf("Title:\t%s\n", title);
+        System.out.printf("Email, phone: %s\t %d \n", email, phoneNumber);
+        System.out.printf("Salary: %d\n", salary);
     }
 }
--- src/main/java/kuklov/JavaCore/Lesson4/Main.java
+++ src/main/java/kuklov/JavaCore/Lesson4/Main.java
@@ -1,4 +1,49 @@
 package kuklov.JavaCore.Lesson4;
 
+import java.util.Random;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Random generator = new Random();
+
+        Employee emp1 = new Employee("Alex", "Alexeevich", "Alexeev", 41,
+                "Manager", 353453453, "alex@company.com", 51000);
+//        Employee emp2 = new Employee("Alex", "Alexeevich", "Alexeev", 41,
+//                "Manager", 353453453, "alex@company.com", 51000);
+//        Employee emp1 = new Employee("Alex", "Alexeevich", "Alexeev", 41,
+//                "Manager", 353453453, "alex@company.com", 51000);
+//        Employee emp1 = new Employee("Alex", "Alexeevich", "Alexeev", 41,
+//                "Manager", 353453453, "alex@company.com", 51000);
+//        Employee emp1 = new Employee("Alex", "Alexeevich", "Alexeev", 41,
+//                "Manager", 353453453, "alex@company.com", 51000);
+//        emp1.showInfo();
+
+        Employee[]  emps = new Employee[5];
+        String[] names = { "Alex", "Mike", "Max", "Benny", "John" };
+        String[] names2 = { "Alexandrovich", "Mikhaylovibch", "Junior", "James", "Ivanovich" };
+        String[] surnames = { "Vazovsky", "Superman", "Batman", "Armstrong", "Bush" };
+        String[] titles = { "Manager", "CEO", "Software engineer", "Postman", "Astronaut"};
+
+        for (int i = 0; i < 5; i++) {
+            String name = names[generator.nextInt(5)];
+            String name2 = names2[generator.nextInt(5)];
+            String surname = surnames[generator.nextInt(5)];
+            int age = generator.nextInt(50) + 18;
+            int salary = generator.nextInt(100000) + 12500;
+            int phonenumber = generator.nextInt(999999) + 1000000;
+            String title = titles[generator.nextInt(5)];
+            String email = name + "_the_" + title + "@company.com";
+
+            emps[i] = new Employee(name, name2, surname, age, title, phonenumber, email, salary);
+        }
+
+        for (Employee ee : emps)    {
+            if (ee.age >= 40)   {
+                ee.showInfo();
+            }
+        }
+
+    }
+
 }
